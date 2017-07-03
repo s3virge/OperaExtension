2a76553e87db373fd59d1306eb418b8389c09bb3
S3ViRGE <kobzarvit@gmail.com>
2017-07-03 19:27:23 +0300

    freshIt opera plugin

--- background.js
+++ background.js
@@ -1,153 +1,124 @@
 /**
  * Created by s3virge on 21.06.17.
  */
-chrome.runtime.onInstalled.addListener(function() 
-{
-	chrome.contextMenus.create({
-	    title: "Вставить разделитель",
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        title: "Вставить разделитель",
         contexts: ["editable"],
         id: "separator",
         documentUrlPatterns: ["*://base.freshit.ua/*"]
     });
-	
-    menuCreateRootItem("Диагностика", "diagnosticsRoot");
-		menuCreateItem("Жесткий диск","errorsOnHdd","diagnosticsRoot");
-		menuCreateItem("Чистка","сleaningIsNecessary","diagnosticsRoot");
-		menuCreateItem("Клавиатура","diagnosticsKeyboard","diagnosticsRoot");
-		menuCreateItem("Прошивка BIOS","diagnosticsBIOS","diagnosticsRoot");
-		menuCreateItem("Матрица","diagnosticsMatrix","diagnosticsRoot");
-		menuCreateItem("Гнездо питания","diagnosticsPowerSupplyConnector","diagnosticsRoot");
-		menuCreateItem("Не включается","DoesNotTurnOn","diagnosticsRoot");
-		menuCreateItem("Следы залития","diagnosticsPour","diagnosticsRoot");
 
-    ////////////////// работы //////////////////////////
-   /* menuCreateRootItem("Добавить Работы", "AddWorks");
-        menuCreateItem("Жесткий диск","workHDD","AddWorks");
-        menuCreateItem("Чистка","workCleaning","AddWorks");
-        menuCreateItem("Клавиатура","workKeyboard","AddWorks");
-        menuCreateItem("Прошивка BIOS","workBIOS","AddWorks");
-        menuCreateItem("BGA пайка","workBGA","AddWorks");*/
-
-	////////////////// ремонт ////////////////////
-    menuCreateRootItem("Ремонт", "repairRoot");
-		menuCreateItem("Жесткий диск","repairHDD","repairRoot");
-		menuCreateItem("Чистка","repairCleaning","repairRoot");
-		menuCreateItem("Клавиатура","repairKeyboard","repairRoot");
-		menuCreateItem("Прошивка BIOS","repairBIOS","repairRoot");
-        menuCreateItem("Матрица","repairMatrix","repairRoot");
-        menuCreateItem("Гнездо питания","repairPowerSupplyConnector","repairRoot");
-		menuCreateItem("Установка ОС","repairInstallOs","repairRoot");
+    CreateDiagnosticsMenu();
+    CreateRepairMenu();
 });
 
-chrome.contextMenus.onClicked.addListener
-(
-    function(info)
-    {    
-        switch(info.menuItemId){
-			case "separator":
-                menuSendMessage("separator");
-            break;
-			
-			//............... диагностка ...............
-			case "diagnosticsKeyboard":
-                menuSendMessage("diagnosticsKeyboard");
-            break;
-			
-			case "errorsOnHdd":
-                menuSendMessage("errorsOnHdd");
-            break;
-			
-			case "сleaningIsNecessary":
-                menuSendMessage("сleaningIsNecessary");
-            break;           
-			
-			case "DoesNotTurnOn":
-                menuSendMessage("DoesNotTurnOn");
-            break;
-			
-			case "diagnosticsPour":
-                menuSendMessage("diagnosticsPour");
-            break;
-			
-			case "diagnosticsBIOS":
-                menuSendMessage("diagnosticsBIOS");
-            break;
+chrome.contextMenus.onClicked.addListener(function (info) {
+        switch (info.menuItemId) {
+            case "separator":
+                menuSendMessage("notype", "separator");
+                break;
+
+            //............... диагностка ...............
+            case "diagnosticsKeyboard":
+                menuSendMessage("diagnostics","diagnosticsKeyboard");
+                break;
+
+            case "errorsOnHdd":
+                menuSendMessage("diagnostics","errorsOnHdd");
+                break;
+
+            case "сleaningIsNecessary":
+                menuSendMessage("diagnostics","сleaningIsNecessary");
+                break;
+
+            case "DoesNotTurnOn":
+                menuSendMessage("diagnostics","DoesNotTurnOn");
+                break;
+
+            case "diagnosticsPour":
+                menuSendMessage("diagnostics","diagnosticsPour");
+                break;
+
+            case "diagnosticsBIOS":
+                menuSendMessage("diagnostics","diagnosticsBIOS");
+                break;
 
             case "diagnosticsMatrix":
-                menuSendMessage("diagnosticsMatrix");
-            break;
+                menuSendMessage("diagnostics","diagnosticsMatrix");
+                break;
 
             case "diagnosticsPowerSupplyConnector":
-                menuSendMessage("diagnosticsPowerSupplyConnector");
-            break;
+                menuSendMessage("diagnostics","diagnosticsPowerSupplyConnector");
+                break;
 
-			//............ ремонт .....................
+            //............ ремонт .....................
             case "repairHDD":
-                menuSendMessage("repairHDD");
-            break;
-			
-			case "repairCleaning":
-                menuSendMessage("repairCleaning");
-            break;
-			
-			case "repairKeyboard":
-                menuSendMessage("repairKeyboard");
-            break;
-			
-			case "repairInstallOs":
-                menuSendMessage("repairInstallOs");
-            break;
+                menuSendMessage("repair","repairHDD");
+                break;
+
+            case "repairCleaning":
+                menuSendMessage("repair","repairCleaning");
+                break;
+
+            case "repairKeyboard":
+                menuSendMessage("repair","repairKeyboard");
+                break;
+
+            case "repairInstallOs":
+                menuSendMessage("repair","repairInstallOs");
+                break;
 
             case "repairBIOS":
-                menuSendMessage("repairBIOS");
+                menuSendMessage("repair","repairBIOS");
                 break;
 
             case "repairMatrix":
-                menuSendMessage("repairMatrix");
+                menuSendMessage("repair", "repairMatrix");
                 break;
 
             case "repairPowerSupplyConnector":
-                menuSendMessage("repairPowerSupplyConnector");
+                menuSendMessage("repair","repairPowerSupplyConnector");
                 break;
 
-			//............... работы ...................
-			/*case "workBGA":
-                menuSendMessage("workBGA");
-            break;
-			
-			case "workKeyboard":
-                menuSendMessage("workKeyboard");
-            break;		
-			
-			case "workHDD":
-                menuSendMessage("workHDD");
-            break;	
-			
-			case "workCleaning":
-                menuSendMessage("workCleaning");
-            break;
+            //............... работы ...................
+            /*case "workBGA":
+             menuSendMessage("workBGA");
+             break;
 
-            case "workBIOS":
-                menuSendMessage("workBIOS");
-            break;*/
-        }        
+             case "workKeyboard":
+             menuSendMessage("workKeyboard");
+             break;
+
+             case "workHDD":
+             menuSendMessage("workHDD");
+             break;
+
+             case "workCleaning":
+             menuSendMessage("workCleaning");
+             break;
+
+             case "workBIOS":
+             menuSendMessage("workBIOS");
+             break;*/
+        }
 
         /*if (info.menuItemId == "1")
-        {
-            //alert("info.menuItemId == 1");
-            chrome.tabs.executeScript
-            (
-                {file: 'jquery-3.2.1.js'},
-                function() 
-                {
-                    chrome.tabs.executeScript({file: 'myscript.js'});
-                }
-            )
-        }
-        else if (info.menuItemId == "2"){
-            //alert("info.menuItemId == 2");
-            chrome.tabs.executeScript({code: 'document.body.style.backgroundColor="#b3ffb3"'});
-        }*/
+         {
+         //alert("info.menuItemId == 1");
+         chrome.tabs.executeScript
+         (
+         {file: 'jquery-3.2.1.js'},
+         function()
+         {
+         chrome.tabs.executeScript({file: 'myscript.js'});
+         }
+         )
+         }
+         else if (info.menuItemId == "2"){
+         //alert("info.menuItemId == 2");
+         chrome.tabs.executeScript({code: 'document.body.style.backgroundColor="#b3ffb3"'});
+         }*/
     }
 );
 
@@ -165,42 +136,80 @@ chrome.contextMenus.onClicked.addListener
 
 //отправляем сообщение в contentscript.js
 /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "cleaning"}, function( ) {
-        alert("resive response from contentscript.js");
-    });
-});
-*/
+ chrome.tabs.sendMessage(tabs[0].id, {greeting: "cleaning"}, function( ) {
+ alert("resive response from contentscript.js");
+ });
+ });
+ */
 
-function menuSendMessage(messageId){
-    chrome.tabs.query
-            (
-                {active: true, currentWindow: true}, 
-                function(tabs) 
-                {
-                    chrome.tabs.sendMessage
-                    (
-                        tabs[0].id, 
-                        {"menuItem": messageId}
-                    );
-                }
-            );
+function menuSendMessage(messageType, messageId) {
+    chrome.tabs.query({active: true, currentWindow: true},
+        function (tabs)
+        {
+            if (messageType == "repair"){
+                chrome.tabs.sendMessage
+                (
+                    tabs[0].id,
+                    {repair: messageId}
+                );
+            }
+            else if (messageType == "diagnostics"){
+                chrome.tabs.sendMessage
+                (
+                    tabs[0].id,
+                    {diagnostics: messageId}
+                );
+            }
+            else if (messageType == "notype"){
+                chrome.tabs.sendMessage
+                (
+                    tabs[0].id,
+                    {notype: messageId}
+                );
+            }
+        }
+    );
 }
 
-function menuCreateRootItem(rootTitle, rootId){
+function menuCreateRootItem(rootTitle, rootId) {
     chrome.contextMenus.create({
-	    title: rootTitle,
+        title: rootTitle,
         contexts: ["editable", "page"],
         id: rootId,
         documentUrlPatterns: ["*://base.freshit.ua/*"]
     });
 }
 
-function menuCreateItem(itemTitle, itemId, parentId){
+function menuCreateItem(itemTitle, itemId, parentId) {
     chrome.contextMenus.create({
-	    title: itemTitle,
+        title: itemTitle,
         contexts: ["editable", "page"],
         parentId: parentId,
         id: itemId,
         documentUrlPatterns: ["*://base.freshit.ua/*"]
-    }); 
+    });
 }
+
+function CreateDiagnosticsMenu() {
+    menuCreateRootItem("Диагностика", "diagnosticsRoot");
+    menuCreateItem("Жесткий диск", "errorsOnHdd", "diagnosticsRoot");
+    menuCreateItem("Чистка", "сleaningIsNecessary", "diagnosticsRoot");
+    menuCreateItem("Клавиатура", "diagnosticsKeyboard", "diagnosticsRoot");
+    menuCreateItem("Прошивка BIOS", "diagnosticsBIOS", "diagnosticsRoot");
+    menuCreateItem("Матрица", "diagnosticsMatrix", "diagnosticsRoot");
+    menuCreateItem("Гнездо питания", "diagnosticsPowerSupplyConnector", "diagnosticsRoot");
+    menuCreateItem("Не включается", "DoesNotTurnOn", "diagnosticsRoot");
+    menuCreateItem("Следы залития", "diagnosticsPour", "diagnosticsRoot");
+}
+
+function CreateRepairMenu() {
+    menuCreateRootItem("Ремонт", "repairRoot");
+    menuCreateItem("Жесткий диск", "repairHDD", "repairRoot");
+    menuCreateItem("Чистка", "repairCleaning", "repairRoot");
+    menuCreateItem("Клавиатура", "repairKeyboard", "repairRoot");
+    menuCreateItem("Прошивка BIOS", "repairBIOS", "repairRoot");
+    menuCreateItem("Матрица", "repairMatrix", "repairRoot");
+    menuCreateItem("Гнездо питания", "repairPowerSupplyConnector", "repairRoot");
+    menuCreateItem("Установка ОС", "repairInstallOs", "repairRoot");
+}
+
--- contentscript.js
+++ contentscript.js
@@ -46,168 +46,136 @@ chrome.runtime.onMessage.addListener(
     function (msg, sender, sendResponse) {
 
         //console.log(sender.tab ? "from a content script: " + sender.tab.url : "from the extension");
-        if (msg.menuItem) {
-            switch (msg.menuItem) {
 
-                case "separator":
-                    $(':focus').append("------------------------------------------------------\n");
-                    $("#form-save-btn").click();
-                    break;
-
-                //................ даигностика ..............
-                case "errorsOnHdd":
-                    $("#diag_rez_input, #rem_rez_input").append(" Программа проверки жесткого диска обнаружила ошибки. Жесткий диск необходимо заменить.");
-                    $("#form-save-btn").click();
-
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id141').click();	//замена hdd
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "сleaningIsNecessary":
-                    $("#diag_rez_input, #rem_rez_input").append(" Система охлаждения запылена. Необходима чистка.");
-                    $("#form-save-btn").click();
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id116').click();	//разборка
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "DoesNotTurnOn":
-                    $("#diag_rez_input, #rem_rez_input").append(" При нажатии на кнопку включения индикация на корпусе не включается, изображение на матрице не появляется.");
-                    $("#form-save-btn").click();
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id116').click();	//разборка
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "diagnosticsKeyboard":
-                    $("#diag_rez_input, #rem_rez_input").append(" Для дальнейшей диагностики необходима замена клавиатуры.");
-                    $("#form-save-btn").click();
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id123').click();	//замена клавы
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "diagnosticsPour":
-                    $("#diag_rez_input, #rem_rez_input").append(" На материнской плате следы попадания жидкости. Для дальнейшей диагностики необходимо устранить последствия попадания жидкости.");
-                    $("#form-save-btn").click();
-
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id157').click();	//отмывка после залития
-                    //$('#prise-id123').click();	//замена клавы
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "diagnosticsBIOS":
-                    $("#diag_rez_input, #rem_rez_input").append(" Необходимо восстановление прошивки BIOS.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "diagnosticsMatrix":
-                    $("#diag_rez_input, #rem_rez_input").append(" Неисправную матрицу необходимо заменить.");
-                    $("#form-save-btn").click();
-                    $('#prise-id117').click();	//разборка крышки
-                    $('#prise-id119').click();	//замена матрицы
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "diagnosticsPowerSupplyConnector":
-                    $("#diag_rez_input, #rem_rez_input").append(" Сломанно гнездо подключения блока питания." +
-                        "Необходима его замена.");
-                    $("#form-save-btn").click();
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id128').click();	//замена гнезда питания
-                    $(".prise-raboti form").submit();
-                    break;
-
-                //................ ремонт .................
-                case "repairKeyboard":
-                    $("#diag_rez_input, #rem_rez_input").append(" Неисправная клавиатура заменена на новую.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "repairCleaning":
-                    $("#diag_rez_input, #rem_rez_input").append(" Система охлаждения почищена, заменена термопаста на чипах.");
-                    $("#form-save-btn").click();
-
-                    $("#form-save-btn").click();
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id116').click();	//разборка
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "repairInstallOs":
-                    $("#diag_rez_input, #rem_rez_input").append(" Установлена лицензионная операционная система с базовым набором программ.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "repairHDD":
-                    $("#diag_rez_input, #rem_rez_input").append(" Неисправные жесткий диск заменён на новый.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "repairBIOS":
-                    $("#diag_rez_input, #rem_rez_input").append(" Выполнено восстановление прошивки BIOS на программаторе.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "repairMatrix":
-                    $("#diag_rez_input, #rem_rez_input").append(" Выполнена замена неисправной матрицы.");
-                    $("#form-save-btn").click();
-                    break;
-
-                case "repairPowerSupplyConnector":
-                    $("#diag_rez_input, #rem_rez_input").append(" Сломанное гнездо подключения блока питания заменено.");
-                    $("#form-save-btn").click();
-                    break;
-
-                //................ работы ................
-                /*case "workCleaning":
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id116').click();	//разборка
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "workBGA":
-                    $('#prise-id154').click();  //чистка
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id534').click();	//снятие компаунда
-                    $('#prise-id133').click();	//пайка BGA
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "workKeyboard":
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id123').click();	//замена клавы
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "workHDD":
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id141').click();	//замена hdd
-                    $(".prise-raboti form").submit();
-                    break;
-
-                case "workBIOS":
-                    $('#prise-id116').click();	//разборка
-                    $('#prise-id162').click();	//восстановление биос
-                    $(".prise-raboti form").submit();
-                    break;*/
-            }
+        if (msg.diagnostics){
+            processDiagnosisMessage(msg.diagnostics);
         }
+        else if (msg.repair){
+            processRepairMessage(msg.repair);
+        }
+        else if (msg.notype == "separator"){
+            $(':focus').append("------------------------------------------------------\n");
+            $("#form-save-btn").click();
 
-        if (msg.message == "search") {
+        }
+        else if (msg.message == "search") {
             $('.global-search input').attr("value", msg.number);
             $('.global-search form').submit();
         }
     }
 );
 
+function processDiagnosisMessage(message){
+    switch(message){
+        case "errorsOnHdd":
+            $("#diag_rez_input, #rem_rez_input").append(" Программа проверки жесткого диска обнаружила ошибки. Жесткий диск необходимо заменить.");
+            $("#form-save-btn").click();
 
+            $('#prise-id116').click();	//разборка
+            $('#prise-id141').click();	//замена hdd
+            $(".prise-raboti form").submit();
+            break;
 
+        case "сleaningIsNecessary":
+            $("#diag_rez_input, #rem_rez_input").append(" Система охлаждения запылена. Необходима чистка.");
+            $("#form-save-btn").click();
+            $('#prise-id154').click();  //чистка
+            $('#prise-id116').click();	//разборка
+            $(".prise-raboti form").submit();
+            break;
 
+        case "DoesNotTurnOn":
+            $("#diag_rez_input, #rem_rez_input").append(" При нажатии на кнопку включения индикация на корпусе не включается, изображение на матрице не появляется.");
+            $("#form-save-btn").click();
+            $('#prise-id154').click();  //чистка
+            $('#prise-id116').click();	//разборка
+            $(".prise-raboti form").submit();
+            break;
 
+        case "diagnosticsKeyboard":
+            $("#diag_rez_input, #rem_rez_input").append(" Для дальнейшей диагностики необходима замена клавиатуры.");
+            $("#form-save-btn").click();
+            $('#prise-id116').click();	//разборка
+            $('#prise-id123').click();	//замена клавы
+            $(".prise-raboti form").submit();
+            break;
 
+        case "diagnosticsPour":
+            $("#diag_rez_input, #rem_rez_input").append(" На материнской плате следы попадания жидкости. Для дальнейшей диагностики необходимо устранить последствия попадания жидкости.");
+            $("#form-save-btn").click();
 
+            $('#prise-id116').click();	//разборка
+            $('#prise-id154').click();  //чистка
+            $('#prise-id157').click();	//отмывка после залития
+            //$('#prise-id123').click();	//замена клавы
+            $(".prise-raboti form").submit();
+            break;
 
+        case "diagnosticsBIOS":
+            $("#diag_rez_input, #rem_rez_input").append(" Необходимо восстановление прошивки BIOS.");
+            $("#form-save-btn").click();
+            break;
+
+        case "diagnosticsMatrix":
+            $("#diag_rez_input, #rem_rez_input").append(" Неисправную матрицу необходимо заменить.");
+            $("#form-save-btn").click();
+            $('#prise-id117').click();	//разборка крышки
+            $('#prise-id119').click();	//замена матрицы
+            $(".prise-raboti form").submit();
+            break;
+
+        case "diagnosticsPowerSupplyConnector":
+            $("#diag_rez_input, #rem_rez_input").append(" Сломанно гнездо подключения блока питания." +
+                "Необходима его замена.");
+            $("#form-save-btn").click();
+            $('#prise-id116').click();	//разборка
+            $('#prise-id128').click();	//замена гнезда питания
+            $(".prise-raboti form").submit();
+            break;
+    }
+
+}
+
+function processRepairMessage(message){
+    switch(message){
+        case "repairKeyboard":
+            $("#diag_rez_input, #rem_rez_input").append(" Неисправная клавиатура заменена на новую.");
+            $("#form-save-btn").click();
+            break;
+
+        case "repairCleaning":
+            $("#diag_rez_input, #rem_rez_input").append(" Система охлаждения почищена, заменена термопаста на чипах.");
+            $("#form-save-btn").click();
+
+            $("#form-save-btn").click();
+            $('#prise-id154').click();  //чистка
+            $('#prise-id116').click();	//разборка
+            $(".prise-raboti form").submit();
+            break;
+
+        case "repairInstallOs":
+            $("#diag_rez_input, #rem_rez_input").append(" Установлена лицензионная операционная система с базовым набором программ.");
+            $("#form-save-btn").click();
+            break;
+
+        case "repairHDD":
+            $("#diag_rez_input, #rem_rez_input").append(" Неисправные жесткий диск заменён на новый.");
+            $("#form-save-btn").click();
+            break;
+
+        case "repairBIOS":
+            $("#diag_rez_input, #rem_rez_input").append(" Выполнено восстановление прошивки BIOS на программаторе.");
+            $("#form-save-btn").click();
+            break;
+
+        case "repairMatrix":
+            $("#diag_rez_input, #rem_rez_input").append(" Выполнена замена неисправной матрицы.");
+            $("#form-save-btn").click();
+            break;
+
+        case "repairPowerSupplyConnector":
+            $("#diag_rez_input, #rem_rez_input").append(" Сломанное гнездо подключения блока питания заменено.");
+            $("#form-save-btn").click();
+            break;
+    }
+}
