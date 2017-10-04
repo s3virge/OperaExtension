ab12c906e194ec1f8080706fd9e2c7e3b4603e78
S3ViRGE <kobzarvit@gmail.com>
2017-10-04 14:21:41 +0300

    added some new items

--- FreshITPlugin/contentscript.js
+++ FreshITPlugin/contentscript.js
@@ -95,10 +95,6 @@ chrome.runtime.onMessage.addListener(
         else if (msg.repair){
             processRepairMessage(msg.repair);
         }
-        /*else if (msg.notype == "separator"){
-            $(':focus').append("------------------------------------------------------\n");
-            // $("#form-save-btn").click();
-        }*/
         else if (msg.message == "search") {
             $('.global-search input').attr("value", msg.number);
             $('.global-search form').submit();
